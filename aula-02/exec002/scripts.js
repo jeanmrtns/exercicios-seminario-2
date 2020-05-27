e4a1f7877b88f21a1ed8dca435792a4bb2dc9cea
Jean <jeanmrtns4@gmail.com>
2020-05-26 21:42:35 -0300

    Exec 003

---
+++ aula-02/exec002/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    console.log('Hello darkness my old friend');
+    alert('Página carregada!');
+
+});
---
+++ aula-02/exec003/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $("#maca").click(function (){
+        alert("Você clicou na maçã!");
+    });
+
+    $("#frase").click(function (){
+        alert("Curtiu a frase?");
+    });
+
+    $("#consertar").click(function (){
+        alert("Conserte");
+    });
+
+    $("#seu").click(function (){
+        alert("seu");
+    });
+
+    $("#cerebro").click(function (){
+        alert("cérebro!");
+    });
+
+});
